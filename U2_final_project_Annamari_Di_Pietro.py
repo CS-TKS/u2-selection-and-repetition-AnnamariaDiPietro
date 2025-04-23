0b57de479e18567ac9c8ccb15e88287cedcf94d3
AnnamariaDiPietro <annamaria.dipietro@thekaustschool.org>
2025-04-23 15:06:05 +0300

    Day 1 code
    This is my final code of day 1.

--- U2_final_project_Annamari_Di_Pietro.py
+++ U2_final_project_Annamari_Di_Pietro.py
@@ -1,1 +1,10 @@
-print("Github works, now I can see code !")
+#welcome and ask user for name
+user_name = input("Hi, welcome to a trivia game of SDG14. What's your name :"). capitalize()
+
+#ask user if they want to play
+user_response = input("Wanna play(yes or no): "). lower()
+positive_response = "yes"
+if user_response == positive_response:
+    print(f"Cool, lets start {user_name}!")
+else:
+    print("Oh well, see you next time.")
